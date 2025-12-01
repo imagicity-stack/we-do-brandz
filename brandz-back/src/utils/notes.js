66eae42081b02c0fa120da09a78cad02405b002c
imagicity-stack <imagicityart@gmail.com>
2025-12-01 10:28:41 +0530

    Delete brandz-back directory

--- brandz-back/src/utils/notes.js
+++
@@ -1,5 +0,0 @@
-export const logNote = (message, context = {}) => {
-  const timestamp = new Date().toISOString();
-  // eslint-disable-next-line no-console
-  console.log(`[${timestamp}] ${message}`, context);
-};
