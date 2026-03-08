24d3ea5202e8514c70bd74d276f791661053e29d
imagicity-stack <imagicityart@gmail.com>
2026-03-09 00:59:21 +0530

    Fix header and footer visibility by restoring component styles

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,18 +1,5 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { detectLocale } from '../src/utils/locale';
+import Home from '../src/pages/Home';
 
 export default function IndexPage() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const locale = detectLocale();
-    router.replace(`/${locale}`);
-  }, [router]);
-
-  return (
-    <main className="main-container" style={{ padding: '48px 0' }}>
-      <p>Redirecting to your localized experience...</p>
-    </main>
-  );
+  return <Home />;
 }
